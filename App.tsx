6209c241b77008f1ae6039e5824fa0482d758c9f
sidar <ilaslansidar@gmail.com>
2023-05-09 23:16:17 +0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>Booking App</Text>
+    </View>
+  )
+}
+
+export default App;
