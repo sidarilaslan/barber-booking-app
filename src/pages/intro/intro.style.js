54ffdbbb739e510fe0de35308916dc3ea68071c1
sidar <ilaslansidar@gmail.com>
2023-05-17 20:11:51 +0300

    feat: login, register, intro, phoneControl page design added

---
+++ src/pages/intro/intro.style.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+export default StyleSheet.create({
+    button: {
+        width: Dimensions.get("window").width * 0.75,
+        marginBottom: 10,
+    },
+});
