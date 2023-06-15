c70e35ccd115e3d364b353a821fd1e4383b17e09
sidar <ilaslansidar@gmail.com>
2023-06-15 23:08:44 +0300

    feat: admin home page added

---
+++ src/pages/admin/home/home.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import styles from './home.style';
+import auth from '@react-native-firebase/auth';
+import CustomButton from '../../../components/customButton';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+const Home = props => {
+    return (
+        <NativeBaseProvider>
+
+            <View style={styles.container}>
+
+                <TouchableWithoutFeedback
+                    onPress={() => {
+                    }}>
+                    <View style={styles.card_settings}>
+                        <Ionicons name={"reader-outline"} size={50} color={"#2e6fb4"} />
+                        <Text style={styles.text_settings}>
+                            Randevular
+                        </Text>
+                    </View>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback
+                    onPress={() => {
+                    }}>
+                    <View style={styles.card_settings}>
+                        <Ionicons name={"people-outline"} size={50} color={"#2e6fb4"} />
+                        <Text style={styles.text_settings}>
+                            Müşteriler
+                        </Text>
+                    </View>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback
+                    onPress={() => {
+                    }}>
+                    <View style={styles.card_settings}>
+                        <Ionicons name={"cut-outline"} size={50} color={"#2e6fb4"} />
+                        <Text style={styles.text_settings}>
+                            Çalışanlar
+                        </Text>
+                    </View>
+                </TouchableWithoutFeedback>
+
+
+                <CustomButton
+                    buttonText={'Çıkış Yap'}
+                    onClick={async () => {
+                        await auth().signOut();
+                        props.navigation.navigate('welcomeStack', {
+                            screen: 'loginScreen',
+                        });
+                    }}
+                />
+            </View>
+        </NativeBaseProvider>
+    );
+};
+
+export default Home;
